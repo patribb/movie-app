a10fe4c5a495efe14a998eb7eff67824622a558e
PatriciaGM <martinez171214@gmail.com>
2021-12-12 07:42:52 +0100

    Fix generate Cast from API

--- app/Http/Livewire/CastIndex.php
+++ app/Http/Livewire/CastIndex.php
@@ -12,21 +12,24 @@ class CastIndex extends Component
 {
     use WithPagination;
 
-    protected $key = '7d7701ad90bc3d4378fbb7bcff41addb';
-
     public $castTMDBId;
     public $castName;
     public $castPosterPath;
 
     public function generateCast()
     {
-        $newCast = Http::get('https://api.themoviedb.org/3/person/1042728?api_key=7d7701ad90bc3d4378fbb7bcff41addb&language=es-EU')->json();
-        Cast::create([
-            'tmdb_id' => $newCast['id'],
-            'name' => $newCast['name'],
-            'slug' => Str::slug($newCast['name']),
-            'poster_path' => $newCast['profile_path'],
-        ]);
+        $newCast = Http::get('https://api.themoviedb.org/3/person/'.$this->castTMDBId.'?api_key=7d7701ad90bc3d4378fbb7bcff41addb&language=es-EU')->json();
+        $cast = Cast::where('tmdb_id', $newCast['id'])->first();
+        if(!$cast) {
+            Cast::create([
+                'tmdb_id' => $newCast['id'],
+                'name' => $newCast['name'],
+                'slug' => Str::slug($newCast['name']),
+                'poster_path' => $newCast['profile_path'],
+            ]);
+        } else {
+            $this->dispatchBrowserEvent('banner-message', ['style' => 'danger', 'message' => 'Cast already exist']);
+        }
     }
 
     public function render()
--- resources/views/livewire/cast-index.blade.php
+++ resources/views/livewire/cast-index.blade.php
@@ -1,25 +1,22 @@
 <section class="container mx-auto p-6 font-mono">
-    <div class="w-full mb-4 p-2 flex justify-end">
-        <div class="mt-10 sm:mt-0">
-            <div class="mt-5 md:mt-0 md:col-span-2">
-                <form>
-                    <div class="shadow overflow-hidden sm:rounded-md">
-                        <div class="px-4 py-5 bg-white sm:p-6">
-                            <div class="grid grid-cols-6 gap-6">
-                                <div class="col-span-6 sm:col-span-3">
-                                    <label for="first-name" class="block text-sm font-medium text-gray-700">Cast TMDB ID</label>
-                                    <input wire:model="castTMDBId" type="text" autocomplete="given-name"
-                                        class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </form>
+    <div class="w-full flex mb-4 p-2 justify-end">
+        <form class="flex space-x-4 shadow bg-white rounded-md m-2 p-2">
+            <div class="p-1 flex items-center">
+                <label for="tmdb_id_g" class="block text-sm font-medium text-gray-700 mr-4">Cast Tmdb Id</label>
+                <div class="relative rounded-md shadow-sm">
+                    <input wire:model="castTMDBId" id="tmdb_id_g" name="tmdb_id_g"
+                        class="px-3 py-2 border border-gray-300 rounded" placeholder="Cast ID" />
+                </div>
             </div>
-        </div>
-        <x-jet-button wire:click="generateCast">Generate Cast</x-jet-button>
+            <div class="p-1">
+                <button type="button" wire:click="generateCast"
+                    class="inline-flex items-center justify-center py-2 px-4 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-green-600 hover:bg-green-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-green-700 transition duration-150 ease-in-out disabled:opacity-50">
+                    <span>Generate</span>
+                </button>
+            </div>
+        </form>
     </div>
+
     <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
       <div class="w-full overflow-x-auto">
         <table class="w-full">
