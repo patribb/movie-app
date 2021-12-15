d2d5a5d76f202723b742eb893d9865f9299f3295
PatriciaGM <martinez171214@gmail.com>
2021-12-15 16:30:53 +0100

    Frontend Layout

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,6 +15,11 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
+    ])
+
+    .postCss('resources/css/front.css', 'public/css', [
+        require('postcss-import'),
+        require('tailwindcss'),
     ]);
 
 if (mix.inProduction()) {
