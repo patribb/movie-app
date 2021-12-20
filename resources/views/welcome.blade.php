a1eeb5e7afd2843743008807bf57fd3aa8843dac
PatriciaGM <martinez171214@gmail.com>
2021-12-20 08:36:05 +0100

    Create controllers Routes and add sections

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,28 @@
 <x-front-layout>
 <main class="max-w-6xl min-h-screen mx-auto mt-6">
-    <section class="mt-4 bg-indigo-300">Movies</section>
-    <section class="mt-4 bg-indigo-300">Episodes</section>
-    <section class="mt-4 bg-indigo-300">Series</section>
+    <section class="p-2 mt-4 bg-gray-200 rounded dark:text-white dark:bg-gray-900">
+        <div class="p-2 m-2 text-2xl font-bold text-center text-indigo-600 dark:text-indigo-300">
+            <h1>Movies</h1>
+        </div>
+        <div class="grid grid-cols-2 gap-4 rounded sm:grid-cols-3 md:grid-cols-4">
+            Movie Card
+        </div>
+    </section>
+    <section class="p-2 mt-4 bg-gray-200 rounded dark:text-white dark:bg-gray-900">
+        <div class="p-2 m-2 text-2xl font-bold text-center text-indigo-600 dark:text-indigo-300">
+            <h1>Episodes</h1>
+        </div>
+        <div class="grid grid-cols-2 gap-4 rounded sm:grid-cols-3 md:grid-cols-4">
+            Episode Card
+        </div>
+    </section>
+    <section class="p-2 mt-4 bg-gray-200 rounded dark:text-white dark:bg-gray-900">
+        <div class="p-2 m-2 text-2xl font-bold text-center text-indigo-600 dark:text-indigo-300">
+            <h1>Series</h1>
+        </div>
+        <div class="grid grid-cols-2 gap-4 rounded sm:grid-cols-3 md:grid-cols-4">
+            Serie Card
+        </div>
+    </section>
 </main>
 </x-front-layout>
