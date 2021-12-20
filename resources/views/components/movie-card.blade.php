854540bfc02ac774351a0ccc2ecdb2e2f1248b1e
PatriciaGM <martinez171214@gmail.com>
2021-12-20 08:53:20 +0100

    Tailwind CSS Movie Card

---
+++ resources/views/components/movie-card.blade.php
@@ -0,0 +1,8 @@
+<div class="p-1 bg-gray-300 group dark:bg-gray-800 shadow-lg-rounded">
+    <div class="relative">
+        {{$image}}
+    </div>
+    <div class="p-2">
+        {{$slot}}
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,94 @@
             <h1>Movies</h1>
         </div>
         <div class="grid grid-cols-2 gap-4 rounded sm:grid-cols-3 md:grid-cols-4">
-            Movie Card
+            <x-movie-card>
+                <x-slot name='image'>
+                    <div class="aspect-w-2 aspect-h-3">
+                        <img src="https://cdn.pixabay.com/photo/2019/10/17/21/17/joker-4557864_960_720.jpg" alt="" class="object-cover">
+                        <div class="absolute top-0 left-0 w-12 h-8 font-bold text-center text-blue-400 bg-gray-800">New</div>
+                    </div>
+                </x-slot>
+                <a href="/">
+                    <div class="font-bold dark:text-white group-hover:text-blue-400">Movie Title</div>
+                </a>
+            </x-movie-card>
+            <x-movie-card>
+                <x-slot name='image'>
+                    <div class="aspect-w-2 aspect-h-3">
+                        <img src="https://cdn.pixabay.com/photo/2019/10/17/21/17/joker-4557864_960_720.jpg" alt="" class="object-cover">
+                        <div class="absolute top-0 left-0 w-12 h-8 font-bold text-center text-blue-400 bg-gray-800">New</div>
+                    </div>
+                </x-slot>
+                <a href="/">
+                    <div class="font-bold dark:text-white group-hover:text-blue-400">Movie Title</div>
+                </a>
+            </x-movie-card>
+            <x-movie-card>
+                <x-slot name='image'>
+                    <div class="aspect-w-2 aspect-h-3">
+                        <img src="https://cdn.pixabay.com/photo/2019/10/17/21/17/joker-4557864_960_720.jpg" alt="" class="object-cover">
+                        <div class="absolute top-0 left-0 w-12 h-8 font-bold text-center text-blue-400 bg-gray-800">New</div>
+                    </div>
+                </x-slot>
+                <a href="/">
+                    <div class="font-bold dark:text-white group-hover:text-blue-400">Movie Title</div>
+                </a>
+            </x-movie-card>
+            <x-movie-card>
+                <x-slot name='image'>
+                    <div class="aspect-w-2 aspect-h-3">
+                        <img src="https://cdn.pixabay.com/photo/2019/10/17/21/17/joker-4557864_960_720.jpg" alt="" class="object-cover">
+                        <div class="absolute top-0 left-0 w-12 h-8 font-bold text-center text-blue-400 bg-gray-800">New</div>
+                    </div>
+                </x-slot>
+                <a href="/">
+                    <div class="font-bold dark:text-white group-hover:text-blue-400">Movie Title</div>
+                </a>
+            </x-movie-card>
+            <x-movie-card>
+                <x-slot name='image'>
+                    <div class="aspect-w-2 aspect-h-3">
+                        <img src="https://cdn.pixabay.com/photo/2019/10/17/21/17/joker-4557864_960_720.jpg" alt="" class="object-cover">
+                        <div class="absolute top-0 left-0 w-12 h-8 font-bold text-center text-blue-400 bg-gray-800">New</div>
+                    </div>
+                </x-slot>
+                <a href="/">
+                    <div class="font-bold dark:text-white group-hover:text-blue-400">Movie Title</div>
+                </a>
+            </x-movie-card>
+            <x-movie-card>
+                <x-slot name='image'>
+                    <div class="aspect-w-2 aspect-h-3">
+                        <img src="https://cdn.pixabay.com/photo/2019/10/17/21/17/joker-4557864_960_720.jpg" alt="" class="object-cover">
+                        <div class="absolute top-0 left-0 w-12 h-8 font-bold text-center text-blue-400 bg-gray-800">New</div>
+                    </div>
+                </x-slot>
+                <a href="/">
+                    <div class="font-bold dark:text-white group-hover:text-blue-400">Movie Title</div>
+                </a>
+            </x-movie-card>
+            <x-movie-card>
+                <x-slot name='image'>
+                    <div class="aspect-w-2 aspect-h-3">
+                        <img src="https://cdn.pixabay.com/photo/2019/10/17/21/17/joker-4557864_960_720.jpg" alt="" class="object-cover">
+                        <div class="absolute top-0 left-0 w-12 h-8 font-bold text-center text-blue-400 bg-gray-800">New</div>
+                    </div>
+                </x-slot>
+                <a href="/">
+                    <div class="font-bold dark:text-white group-hover:text-blue-400">Movie Title</div>
+                </a>
+            </x-movie-card>
+            <x-movie-card>
+                <x-slot name='image'>
+                    <div class="aspect-w-2 aspect-h-3">
+                        <img src="https://cdn.pixabay.com/photo/2019/10/17/21/17/joker-4557864_960_720.jpg" alt="" class="object-cover">
+                        <div class="absolute top-0 left-0 w-12 h-8 font-bold text-center text-blue-400 bg-gray-800">New</div>
+                    </div>
+                </x-slot>
+                <a href="/">
+                    <div class="font-bold dark:text-white group-hover:text-blue-400">Movie Title</div>
+                </a>
+            </x-movie-card>
         </div>
     </section>
     <section class="p-2 mt-4 bg-gray-200 rounded dark:text-white dark:bg-gray-900">
